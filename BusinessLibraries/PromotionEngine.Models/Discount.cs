88b25a8cc6cb613cd39ad4edebef30eea066b335
chelladurai kumar <k.chelladurai@halsystech.com>
2020-10-05 05:03:38 -0400

    Discount, DiscountCombinations and ShoppingCart Models developed.

---
+++ BusinessLibraries/PromotionEngine.Models/Discount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Models
+{
+    public class Discount
+    {
+        public Discount(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+        public string Description { get; }
+
+        public DiscountCombination DiscountCombination { get; private set; }
+
+      
+    }
+}
---
+++ BusinessLibraries/PromotionEngine.Models/DiscountCombination.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Models
+{
+    public class DiscountCombination
+    {
+        public IEnumerable<KeyValuePair<Product, int>> ItemsCombinations { get; set; }
+        public decimal DiscountAmount { get; set; }
+
+        public DiscountCombination(IEnumerable<KeyValuePair<Product, int>> itemsCombinations,
+            decimal discountAmount)
+        {
+
+            ItemsCombinations = itemsCombinations;
+            DiscountAmount = discountAmount;
+
+        }
+    }
+}
