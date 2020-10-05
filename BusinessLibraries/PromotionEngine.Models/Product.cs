e618d7a4a3cc0c77409507bf1703a2e0938fb697
chelladurai kumar <k.chelladurai@halsystech.com>
2020-10-05 04:57:24 -0400

    Models Project added and New product model added.

---
+++ BusinessLibraries/PromotionEngine.Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PromotionEngine.Models
+{
+    public class Product
+    {
+        public Product(int id, string name, string description, decimal cost)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Cost = cost;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public decimal Cost { get; }
+
+
+    }
+}
