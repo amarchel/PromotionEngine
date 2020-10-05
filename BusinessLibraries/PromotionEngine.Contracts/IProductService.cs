2bc1c4cfd84448b313ef83a9291885d4d9e3f048
chelladurai kumar <k.chelladurai@halsystech.com>
2020-10-05 05:16:23 -0400

    Added services and contracts Projects. Product Service implemented.

---
+++ BusinessLibraries/PromotionEngine.Contracts/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using PromotionEngine.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Contracts
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> GetAll();
+        Product Get(int id);
+        Product CreateProduct(string name, string description, decimal costPerUnit);
+        
+    }
+}
---
+++ BusinessLibraries/PromotionEngine.Services/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using PromotionEngine.Contracts;
+using PromotionEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PromotionEngine.Services
+{
+    public class ProductService : IProductService
+    {
+        readonly List<Product> products = new List<Product>();
+        public Product CreateProduct(string name, string description, decimal cost)
+        {
+            int maxId = products.Select(c => c.Id)
+               .DefaultIfEmpty(0).Max();
+            products.Add(new Product(++maxId, name, description, cost));
+            return products[--maxId];
+        }
+
+        public Product Get(int id)
+        {
+            return products.First(p => p.Id == id);
+            
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return products;
+        }
+    }
+}
