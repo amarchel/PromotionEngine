8b98e698b64344a3023e6b5c3d1bf63e1189f740
chelladurai kumar <k.chelladurai@halsystech.com>
2020-10-05 04:33:51 -0400

    New Project Added

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace PromotionEngine
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+
+            var services = new ServiceCollection();
+
+            Startup.Configure(services);
+            
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            
+
+
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PromotionEngine
+{
+   internal static class Startup
+    {
+        internal static void Configure(IServiceCollection services) { 
+        }
+    }
+}
